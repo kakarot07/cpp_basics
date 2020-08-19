f2a938612d61615261a171bb7dcc175bbefd43fe
Anas Husain Khan <47828050+kakarot07@users.noreply.github.com>
2020-08-19 21:16:51 +0530

    Update profit_loss.cpp

--- ifelse/profit_loss.cpp
+++ ifelse/profit_loss.cpp
@@ -1,4 +1,4 @@
-//Write a C program to calculate profit and loss on a transaction.
+//Write a C program to calculate profit and loss on a transaction..
 #include<iostream>
 using namespace std;
 int main(){
